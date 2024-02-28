0971b21e02d8706050fe9bfcd541f13d8743842a
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-02-28 13:49:40 +0100

    edited custom-quiz page

--- pages/custom-quiz.jsx
+++ pages/custom-quiz.jsx
@@ -1,4 +1,6 @@
+// import { addCustomQuiz, editCustomQuiz, removeCustomQuiz, toggleCompleteQuiz } from "@redux/CustomQuizPage";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function CustomQuizPage() {
   const [question, setQuestion] = useState("");
@@ -7,15 +9,38 @@ export default function CustomQuizPage() {
     setQuestion(event.target.value);
   }
 
+  function handleAddQuestion() {
+    dispatch(addCustomQuiz(question));
+  }
+
+  function toggleCompleteQuiz() {
+    dispatch(toggleCompleteQuiz());
+  }
+
+  function handleEditQuestion() {
+    dispatch(editCustomQuiz(question));
+  }
+
+  function handleRemoveQuestion() {
+    dispatch(removeCustomQuiz(question));
+  }
+
+
   return (
-    <div className="flex flex-col">
-      <h1 className="flex justify-center">admin page</h1>
-      <input
-        onChange={handleInputChange}
-        type="text"
-        placeholder="enter your question here"
-        value={question}
-      ></input>
+    <div>
+      <div className="flex flex-col">
+        <h1 className="flex justify-center">Custom-quiz page</h1>
+        <input
+          onChange={handleInputChange}
+          type="text"
+          placeholder="enter your question here"
+          value={question}
+        ></input>
+        <button onClick={handleAddQuestion}>Add question</button>
+      </div>
+      <div>
+        <ul></ul>
+      </div>
     </div>
   );
 }
