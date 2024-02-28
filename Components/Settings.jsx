528f0dd909654a982fe9a150f6e124295c1cfc11
Axel Kacou Yhalin <axelkacou1@gmail.com>
2024-02-28 10:06:20 +0100

    ..

---
+++ Components/Settings.jsx
@@ -0,0 +1,14 @@
+import { useSelector, useDispatch } from "react-redux";
+const questionCategory = useSelector(
+  (state) => state.options.question_category
+);
+const questionDifficulty = useSelector(
+  (state) => state.options.question_difficulty
+);
+
+
+const dispatch = useDispatch()
+
+export default function Settings() {
+  return <div></div>;
+}
---
+++ redux/ations.js
@@ -0,0 +1,11 @@
+export const changeCategory = (value) => ({
+  type: "CHANGE_CATEGORY",
+  value,
+});
+
+export const changeDifficulty = (value) => ({
+  type: "CHANGE_DIFFICULTY",
+  value,
+});
+
+
