44720b3b958ed64c554e0080702d264cf7765244
shantihedelin <145372686+shantihedelin@users.noreply.github.com>
2024-02-29 13:58:21 +0100

    working and angry

--- pages/custom-quiz.jsx
+++ pages/custom-quiz.jsx
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addCustomQuiz, setCurrentQuiz } from "@/redux/CustomQuizSlice";
+import { addCustomQuiz, setCurrentQuiz, selectAllFinishedQuizzes } from "@/redux/CustomQuizSlice";
 import AddQuestionForm from "@/Components/AddQuestionForm";
+import CustomQuizList from "@/Components/CustomQuizList";
 
 export default function CustomQuizPage() {
   const dispatch = useDispatch();
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState([]); // State to store all the questions
+  const [quizName, setQuizName] = useState(""); // State to store quiz name
+  const [questions, setQuestions] = useState([]); // State to store questions
 
+  
+  const finishedQuizzes = useSelector(selectAllFinishedQuizzes); // Fetch the questions from redux store
+  
   const handleAddQuestion = (newQuestionData) => {
     const updatedQuestions = [...questions, newQuestionData]; // Adds the new question to the list
     setQuestions(updatedQuestions); // update the state
@@ -15,11 +20,26 @@ export default function CustomQuizPage() {
   };
 
   const handleMakeQuiz = () => {
-    //TODO: Add logic to make the quiz
+  if (questions.length === 0 || quizName.trim() === "") return;
+
+const newQuiz = {
+  name: quizName,
+  questions: [...questions],
+};
+
+    dispatch(setCurrentQuiz(newQuiz));
+    setQuizName("");
+    setQuestions([]);
   };
 
   return (
     <div>
+      <input
+      type="text"
+      placeholder="Enter Quiz Name"
+      value={quizName}
+      onChange={(e) => setQuizName(e.target.value)}
+      />
       <AddQuestionForm
         onAddQuestion={handleAddQuestion}
         newQuestion={newQuestion}
@@ -33,6 +53,7 @@ export default function CustomQuizPage() {
         </ul>
       </div>
       <button onClick={handleMakeQuiz}>Make Quiz</button>
+      <CustomQuizList/>
     </div>
   );
 }
