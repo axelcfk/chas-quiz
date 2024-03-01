eaf790758da4e0e493dafaa48975086992cec6a0
afzbehroz <145428759+afzbehroz@users.noreply.github.com>
2024-03-01 14:18:45 +0100

    Styling på make your quiz sidan

--- Components/AddQuestionForm.js
+++ Components/AddQuestionForm.js
@@ -30,19 +30,21 @@ function AddQuestionForm({ onAddQuestion, newQuestion, setNewQuestion }) {
   };
 
   return (
-    <div>
-      <label>
+    <div className="grid grid-cols- grid-rows- gap-4 justify-items-center">
+      <label className="text-center text-2xl">
         Question:
         <input
+          className="w-full border-gray-300 mb-4 px-3 py-3 rounded-3xl border-solid focus:outline-none text-center"
           type="text"
           placeholder="Enter question"
           value={newQuestion}
           onChange={(e) => setNewQuestion(e.target.value)}
         />
       </label>
+    <div className="grid grid-cols-2">
       <label>
-        Correct answer:
         <input
+          className="w-full mt-1 px- py-2 rounded-lg border border-gray-300 focus:outline-none border-solid focus:border-blue-500"
           type="text"
           placeholder="Correct answer"
           value={correctAnswer}
@@ -50,36 +52,29 @@ function AddQuestionForm({ onAddQuestion, newQuestion, setNewQuestion }) {
         />
       </label>
       <label>
-        Wrong Answers:
         {wrongAnswers.map((answer, index) => (
-            // creates a new <input> for each element in the array "wrongAnswers",
-            // which is 3, instead of writing same code 3 times
           <input
             key={index}
+            className="w-full mt-1 px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500"
             type="text"
             value={answer}
-            onChange={(event) => handleWrongAnswerChange(index, event.target.value)}
+            placeholder="Wrong answer"
+            onChange={(event) =>
+              handleWrongAnswerChange(index, event.target.value)
+            }
           />
         ))}
       </label>
-      <button class type="button" onClick={handleAddQuestion}>
+      </div>
+      <button
+        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline border-none"
+        type="button"
+        onClick={handleAddQuestion}
+      >
         Add Question
       </button>
-      
-      <ul>
-        <li>
-            <strong>Question:</strong>{newQuestion}
-        </li>
-        <li>
-            <strong>Correct answer:</strong>{correctAnswer}
-        </li>
-        <li>
-            <strong>Wrong Answers:</strong>{wrongAnswers.filter((answer) => answer.trim()!== "").join("", )}
-        </li>
-      </ul>
     </div>
   );
 }
 
 export default AddQuestionForm;
-
--- pages/custom-quiz.jsx
+++ pages/custom-quiz.jsx
@@ -1,26 +1,21 @@
 import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import {
   addCustomQuiz,
-  setCurrentQuiz,
-  selectAllFinishedQuizzes,
+  setCurrentQuiz
 } from "@/redux/CustomQuizSlice";
 import AddQuestionForm from "@/Components/AddQuestionForm";
 import CustomQuizList from "@/Components/customQuizList";
 
 export default function CustomQuizPage() {
   const [newQuestion, setNewQuestion] = useState("");
-  const [quizName, setQuizName] = useState(""); // State to store quiz name
-  const [questions, setQuestions] = useState([]); // State to store questions in array
-
+  const [quizName, setQuizName] = useState("");
+  const [questions, setQuestions] = useState([]);
   const dispatch = useDispatch();
- 
-
-
 
   const handleAddQuestion = (newQuestionData) => {
-    const updatedQuestions = [...questions, newQuestionData]; // Adds the new question to the list
-    setQuestions(updatedQuestions); // update the state
+    const updatedQuestions = [...questions, newQuestionData];
+    setQuestions(updatedQuestions);
   };
 
   const handleMakeQuiz = () => {
@@ -28,51 +23,49 @@ export default function CustomQuizPage() {
 
     const newQuiz = {
       name: quizName,
-      results: [...questions],
+      results: [...questions]
     };
 
-    dispatch(addCustomQuiz(newQuiz)); // Add quiz to redux store
+    dispatch(addCustomQuiz(newQuiz));
     dispatch(setCurrentQuiz(newQuiz));
-    // When click on "Make Quiz" button, reset the form
+
     setQuizName("");
     setQuestions([]);
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="grid grid-cols- place-items-center h-screen">
       <div className="w-full max-w-md p-40 bg-white rounded-lg shadow-md">
-        <input
-          className="w-full mb-4 px-3 py-2 rounded-lg border-solid border-green-400 focus:outline-none"
-            type="text"
-            placeholder="Enter Quiz Name"
-            value={quizName}
-            onChange={(e) => setQuizName(e.target.value)}
-        />
+  <input
+    className="w-full border-gray-300 mb-4 px-3 py-3 rounded-3xl border-solid focus:outline-none text-center"
+    type="text"
+    placeholder="Enter Quiz Name"
+    value={quizName}
+    onChange={(e) => setQuizName(e.target.value)}
+  />
         <AddQuestionForm
           onAddQuestion={(newQuestionData) => {
-          handleAddQuestion(newQuestionData);
-          dispatch(addCustomQuiz(newQuestionData)); // Dispatch action to add question to Redux store
-        }}
+            handleAddQuestion(newQuestionData);
+            dispatch(addCustomQuiz(newQuestionData));
+          }}
           newQuestion={newQuestion}
           setNewQuestion={setNewQuestion}
         />
-        <div className="mt-4">
-          <ul>
+        <div className="mt-6 p-1">
+          <ul className="space-y-1 text-lg">
             {questions.map((question, index) => (
-              <li key={index}>{question.question}</li>
+              <li key={index}>Question {question.question}</li>
             ))}
           </ul>
         </div>
         <button
-          className="w-full mt-4 bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="block mx-auto mt-4 hover:bg-blue-400 bg-blue-500 text-white font-bold py-2.5 px-14 rounded focus:outline-none focus:shadow-outline border-none"
           onClick={handleMakeQuiz}
         >
           Make Quiz
         </button>
         <CustomQuizList />
       </div>
-      <button onClick={handleMakeQuiz}>Make Quiz</button>
-      <CustomQuizList /> //? Maybe send props?
     </div>
   );
 }
